a645fd42f029bc6a7c030582dccf8842809d9c70
Michael Sichico <michaelsichico@gmail.com>
2014-07-11 17:40:55 -0800

    css, javascript, & jquery

---
+++ dynamic/atom/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="bootstrap-3.2.0-dist/css/bootstrap.min.css">
+		
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="bootstrap-3.2.0-dist/css/bootstrap-theme.min.css">
+		
+		<!-- jQuery CSS -->
+		<link rel="stylesheet" href="jquery-ui-1.11.0/jquery-ui.css"/>
+		
+		<!-- FontAwesome -->
+		<link rel="stylesheet" href="font-awesome-4.1.0/css/font-awesome.css"/>
+		
+		<!-- jQuery -->
+		<script src="jquery-2.1.1/jquery-2.1.1.min.js"></script>
+		
+		<!-- jQuery UI -->
+		<script src="jquery-ui-1.11.0/jquery-ui.js"></script>
+		
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+	
+	</head>
+		
+	<body>
+		
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#">About Us</a></li>
+					<li><a href="#">FAQ</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+		</nav> <!-- end main navigation -->
+		
+		<div class="container">
+			<h1>LifeBox ang ASTIG!</h1>
+		</div>
+		
+		<footer id="footer">
+			<div class="container">
+				<p>LifeBox UE-Manila</p>
+			</div>
+		</footer>
+		
+	</body>	
+	
+</html>
